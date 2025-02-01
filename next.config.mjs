1bed61d1f4e8dc144f1454d95e2dc02185a8794f
AnthonyJJ <anthonyjj89@example.com>
2025-02-01 18:56:19 +0400

    fix: simplify next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
   images: {
     domains: ['cdn.discordapp.com'],
-  },
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    MONGODB_DB: process.env.MONGODB_DB,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   async headers() {
     return [
